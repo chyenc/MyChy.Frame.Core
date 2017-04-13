49b7f7b7e1b57a6f6fb384caad262d5f1272cd73
MyChy <chyenc@gmail.com>
2017-04-13 15:56:50 +0800

    增加底层框架类

---
+++ MyChy.Frame.Core.Web/MyChy.Frame.Core.Common/Core/IAssemblyProvider.cs
@@ -0,0 +1,16 @@
+﻿using MyChy.Frame.Core.Common.Core.Modules;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MyChy.Frame.Core.Common.Core
+{
+    public interface IAssemblyProvider
+    {
+        IEnumerable<Assembly> GetAssemblies(string path);
+
+
+        IEnumerable<ModuleInfo> GetModules(string path);
+    }
+}
