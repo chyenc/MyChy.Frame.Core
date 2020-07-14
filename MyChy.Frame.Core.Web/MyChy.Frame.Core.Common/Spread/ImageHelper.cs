aa897ba46ee625d1e0be62ea8ba70d9e44e8bcbd
陈岩 <chyenx@gmail.com>
2020-07-14 10:00:09 +0800

    更新生成缩微图功能

---
+++ MyChy.Frame.Core.Web/MyChy.Frame.Core.Common/Spread/ImageHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+
+namespace MyChy.Frame.Core.Common.Spread
+{
+    internal static class ImageHelper
+    {
+        public static Image GetThumbnailImage(Image image, int width, int height)
+        {
+            if (image == null || width < 1 || height < 1)
+                return null;
+
+            // 新建一个bmp图片
+            //
+            Image bitmap = new Bitmap(width, height);
+
+            // 新建一个画板
+            //
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+
+                // 设置高质量插值法
+                //
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+                // 设置高质量,低速度呈现平滑程度
+                //
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                // 高质量、低速度复合
+                //
+                g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+
+                // 清空画布并以透明背景色填充
+                //
+                g.Clear(Color.Transparent);
+
+                // 在指定位置并且按指定大小绘制原图片的指定部分
+                //
+                g.DrawImage(image, new Rectangle(0, 0, width, height),
+                    new Rectangle(0, 0, image.Width, image.Height),
+                    GraphicsUnit.Pixel);
+
+                return bitmap;
+            }
+
+        }
+
+        /// <summary>
+        /// 获取图像编码解码器的所有相关信息
+        /// </summary>
+        /// <param name="mimeType">包含编码解码器的多用途网际邮件扩充协议 (MIME) 类型的字符串</param>
+        /// <returns>返回图像编码解码器的所有相关信息</returns>
+        public static ImageCodecInfo GetCodecInfo(string mimeType)
+        {
+            var codecInfo = ImageCodecInfo.GetImageEncoders();
+            return codecInfo.FirstOrDefault(ici => ici.MimeType == mimeType);
+        }
+
+        public static ImageCodecInfo GetImageCodecInfo(ImageFormat format)
+        {
+            var encoders = ImageCodecInfo.GetImageEncoders();
+
+            return encoders.FirstOrDefault(icf => icf.FormatID == format.Guid);
+        }
+
+        /// <summary>
+        /// 高质量保存图片
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="savePath"></param>
+        /// <param name="ici"></param>
+        public static void SaveImage(Image image, string savePath, ImageCodecInfo ici)
+        {
+            // 设置 原图片 对象的 EncoderParameters 对象
+            //
+            var parms = new EncoderParameters(1);
+            var parm = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, ((long)95));
+            parms.Param[0] = parm;
+
+            image.Save(savePath, ici, parms);
+            parms.Dispose();
+        }
+    }
+}
---
+++ MyChy.Frame.Core.Web/MyChy.Frame.Core.Common/Spread/Thumbnail.cs
@@ -0,0 +1,178 @@
+﻿using MyChy.Frame.Core.Common.Helper;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace MyChy.Frame.Core.Common.Spread
+{
+    public sealed class Thumbnail
+    {
+ 
+        public Thumbnail(string file)
+        {
+            FilePath = file;
+        }
+
+        #region 属性
+
+        /// <summary>
+        /// 是否使用缩微图文件夹
+        /// </summary>
+        public bool IsthumbnailImage { get; set; } = false;
+
+
+        /// <summary>
+        /// 图片宽
+        /// </summary>
+        public int Width { get; set; } = 100;
+
+        /// <summary>
+        /// 图片高
+        /// </summary>
+        public int Height { get; set; } = 100;
+
+
+        /// <summary>
+        /// 图片文件地址
+        /// </summary>
+        public string FilePath { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 缩微图文件夹
+        /// </summary>
+        public string ThumbnailImage { get; set; } = "ThumbnailImages";
+
+        public Thumbnail()
+        {
+
+        }
+
+        #endregion
+
+        #region 共有方法
+
+        /// <summary>
+        /// 生成缩略图 提供文件
+        /// </summary>
+        /// <param name="newfile">文件</param>
+        public void ThumbnailFile(string newfile)
+        {
+            ThumbnailFileThread(newfile, Width, Height);
+        }
+
+        /// <summary>
+        /// 生成缩略图 提供文件
+        /// </summary>
+        /// <param name="newfile"></param>
+        /// <param name="width">生成的缩微图的宽</param>
+        /// <param name="height">生成的缩微图的高</param>
+        public void ThumbnailFile(string newfile, int width, int height)
+        {
+            ThumbnailFileThread(newfile, width, height);
+        }
+
+        #endregion
+
+        #region 私有方法
+
+        private void ThumbnailFileThread(string newfile, int width, int height)
+        {
+            Width = width;
+            Height = height;
+
+            if (IsthumbnailImage)
+            {
+                FilePath = Path.Combine(Path.GetDirectoryName(newfile), ThumbnailImage);
+            }
+            else
+            {
+                FilePath = newfile;
+            }
+
+            FileHelper.CreatedFolder(FilePath);
+
+            if (string.IsNullOrEmpty(FilePath)) return;
+
+            var trd = new Thread(new ThreadStart(TimedProgress));
+            trd.Start();
+
+            //TimedProgress();
+        }
+
+        //根据原图片,缩微图大小等比例缩放 文件
+        private void TimedProgress()
+        {
+            var icf = ImageHelper.GetImageCodecInfo(ImageFormat.Jpeg);
+
+            if (!SafeCheckHelper.IsJpgStr(FilePath)) return;
+            //var extension = IoFiles.GetExtension(File);
+            try
+            {
+                Image image;
+                using (image = Image.FromFile(FilePath))
+                {
+                    var imageSize = GetImageSize(image);
+
+                    Image thumbnailImage;
+                    using (thumbnailImage = ImageHelper.GetThumbnailImage(image, imageSize.Width, imageSize.Height))
+                    {
+                        //var thumbnailImageFilename = IsthumbnailImage ? FilePath : Newfile;
+
+                        ImageHelper.SaveImage(thumbnailImage, FilePath, icf);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Log(ex);
+            }
+        }
+
+        /// <summary>
+        /// 等比例求出缩微图大小
+        /// </summary>
+        /// <param name="picture"></param>
+        /// <returns></returns>
+        private Size GetImageSize(Image picture)
+        {
+            Size imageSize;
+
+            imageSize = new Size(Width, Height);
+
+            if ((picture.Height > imageSize.Height) || (picture.Width > imageSize.Width))
+            {
+
+                double heightRatio = (double)picture.Height / picture.Width;
+                double widthRatio = (double)picture.Width / picture.Height;
+
+                int desiredHeight = imageSize.Height;
+                int desiredWidth = imageSize.Width;
+
+
+                imageSize.Height = desiredHeight;
+                if (widthRatio > 0)
+                    imageSize.Width = Convert.ToInt32(imageSize.Height * widthRatio);
+
+                if (imageSize.Width > desiredWidth)
+                {
+                    imageSize.Width = desiredWidth;
+                    imageSize.Height = Convert.ToInt32(imageSize.Width * heightRatio);
+                }
+            }
+            else
+            {
+                imageSize.Width = picture.Width;
+                imageSize.Height = picture.Height;
+            }
+
+            return imageSize;
+        }
+
+        #endregion
+
+    }
+}
